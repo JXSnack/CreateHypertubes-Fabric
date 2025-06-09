ceea6034ec973923cd4dd80991ac05fb5cb82dc2
JXSnack <jx.snack@gmail.com>
2025-06-09 23:52:25 +0200

    port MathUtils

--- src/main/java/com/pedrorok/hypertube/utils/MathUtils.java
+++ src/main/java/com/pedrorok/hypertube/utils/MathUtils.java
@@ -1,6 +1,6 @@
 package com.pedrorok.hypertube.utils;
 
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.util.math.Vec3d;
 
 /**
  * @author Rok, Pedro Lucas nmm. Created on 08/05/2025
@@ -9,7 +9,7 @@ import net.minecraft.world.phys.Vec3;
 public class MathUtils {
 
 
-    public static float getMediumSpeed(Vec3 velocity) {
+    public static float getMediumSpeed(Vec3d velocity) {
         float x = Math.abs((float) velocity.x);
         float y = Math.abs((float) velocity.y);
         float z = Math.abs((float) velocity.z);
