33d4669e55e1f872ec473c7b400db13920e7ae6c
JXSnack <jx.snack@gmail.com>
2025-06-09 23:57:51 +0200

    port almost finished except for finding a replacement for StreamCodec

--- src/main/java/com/pedrorok/hypertube/utils/CodecUtils.java
+++ src/main/java/com/pedrorok/hypertube/utils/CodecUtils.java
@@ -2,7 +2,7 @@ package com.pedrorok.hypertube.utils;
 
 import io.netty.buffer.ByteBuf;
 import net.minecraft.network.codec.StreamCodec;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.util.math.Vec3d;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -14,14 +14,14 @@ import java.util.List;
  */
 public class CodecUtils {
 
-    public static StreamCodec<ByteBuf, Vec3> VEC3 = new StreamCodec<>() {
+    public static StreamCodec<ByteBuf, Vec3d> VEC3 = new StreamCodec<>() {
         @Override
-        public @NotNull Vec3 decode(ByteBuf buffer) {
-            return new Vec3(buffer.readDouble(), buffer.readDouble(), buffer.readDouble());
+        public @NotNull Vec3d decode(ByteBuf buffer) {
+            return new Vec3d(buffer.readDouble(), buffer.readDouble(), buffer.readDouble());
         }
 
         @Override
-        public void encode(ByteBuf buffer, Vec3 value) {
+        public void encode(ByteBuf buffer, Vec3d value) {
             buffer.writeDouble(value.x);
             buffer.writeDouble(value.y);
             buffer.writeDouble(value.z);
@@ -29,11 +29,11 @@ public class CodecUtils {
 
     };
 
-    public static StreamCodec<ByteBuf, List<Vec3>> VEC3_LIST = new StreamCodec<>() {
+    public static StreamCodec<ByteBuf, List<Vec3d>> VEC3_LIST = new StreamCodec<>() {
         @Override
-        public List<Vec3> decode(ByteBuf byteBuf) {
+        public List<Vec3d> decode(ByteBuf byteBuf) {
             int size = byteBuf.readInt();
-            List<Vec3> vec3s = new ArrayList<>(size);
+            List<Vec3d> vec3s = new ArrayList<>(size);
             for (int i = 0; i < size; i++) {
                 vec3s.add(VEC3.decode(byteBuf));
             }
@@ -41,9 +41,9 @@ public class CodecUtils {
         }
 
         @Override
-        public void encode(ByteBuf o, List<Vec3> vec3s) {
+        public void encode(ByteBuf o, List<Vec3d> vec3s) {
             o.writeInt(vec3s.size());
-            for (Vec3 vec3 : vec3s) {
+            for (Vec3d vec3 : vec3s) {
                 VEC3.encode(o, vec3);
             }
         }
