7fe46ff9e97704e675a361d2a645b14c86fb906f
JXSnack <jx.snack@gmail.com>
2025-06-10 00:17:34 +0200

    fix imports, of course

--- src/main/java/com/pedrorok/hypertube/blocks/TubeConnection.java
+++ src/main/java/com/pedrorok/hypertube/blocks/TubeConnection.java
@@ -1,11 +1,8 @@
 package com.pedrorok.hypertube.blocks;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.WorldAccess;
-import net.minecraft.world.level.LevelAccessor;
 
 /**
  * @author Rok, Pedro Lucas nmm. Created on 21/04/2025
