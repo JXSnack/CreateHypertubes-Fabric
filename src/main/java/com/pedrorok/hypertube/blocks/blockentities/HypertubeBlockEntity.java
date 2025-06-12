7909e3d4176b1060f8ec2ae2ab8886c6c269b0b3
JXSnack <jx.snack@gmail.com>
2025-06-12 22:37:12 +0200

    port HypertubeBlockEntity

--- src/main/java/com/pedrorok/hypertube/blocks/blockentities/HypertubeBlockEntity.java
+++ src/main/java/com/pedrorok/hypertube/blocks/blockentities/HypertubeBlockEntity.java
@@ -5,17 +5,16 @@ import com.pedrorok.hypertube.blocks.IBezierProvider;
 import com.pedrorok.hypertube.managers.placement.BezierConnection;
 import com.pedrorok.hypertube.managers.placement.SimpleConnection;
 import lombok.Getter;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.nbt.CompoundTag;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtOps;
-import net.minecraft.network.Connection;
-import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.AABB;
+import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -37,28 +36,28 @@ public class HypertubeBlockEntity extends BlockEntity implements IBezierProvider
 
     public void setConnectionTo(BezierConnection connection) {
         this.connectionTo = connection;
-        if (level != null && !level.isClientSide()) {
-            if (level.getBlockState(worldPosition).getBlock() instanceof HypertubeBlock hypertubeBlock) {
-                hypertubeBlock.updateBlockStateFromEntity(level, worldPosition);
+        if (world != null && !world.isClient()) {
+            if (world.getBlockState(pos).getBlock() instanceof HypertubeBlock hypertubeBlock) {
+                hypertubeBlock.updateBlockStateFromEntity(world, pos);
             }
         }
-        setChanged();
+        markDirty();
         sync();
     }
 
     public void setConnectionFrom(SimpleConnection connectionFrom, Direction direction) {
         this.connectionFrom = connectionFrom;
 
-        if (level != null && !level.isClientSide()) {
-            if (level.getBlockState(worldPosition).getBlock() instanceof HypertubeBlock hypertubeBlock) {
-                hypertubeBlock.updateBlockStateFromEntity(level, worldPosition);
+        if (world != null && !world.isClient()) {
+            if (world.getBlockState(pos).getBlock() instanceof HypertubeBlock hypertubeBlock) {
+                hypertubeBlock.updateBlockStateFromEntity(world, pos);
                 if (direction != null) {
                     BlockState state = hypertubeBlock.getState(List.of(direction));
-                    hypertubeBlock.updateBlockState(level, worldPosition, state);
+                    hypertubeBlock.updateBlockState(world, pos, state);
                 }
             }
         }
-        setChanged();
+        markDirty();
         sync();
     }
 
@@ -67,19 +66,19 @@ public class HypertubeBlockEntity extends BlockEntity implements IBezierProvider
     }
 
     public void sync() {
-        if (level != null && !level.isClientSide) {
-            level.sendBlockUpdated(worldPosition, getBlockState(), getBlockState(), Block.UPDATE_ALL);
+        if (world != null && !world.isClient) {
+            world.updateListeners(pos, getCachedState(), getCachedState(), Block.NOTIFY_ALL);
         }
     }
 
 
     @Override
-    protected void saveAdditional(CompoundTag tag) {
-        super.saveAdditional(tag);
+    protected void writeNbt(NbtCompound tag) {
+        super.writeNbt(tag);
         writeConnection(tag);
     }
 
-    private void writeConnection(CompoundTag tag) {
+    private void writeConnection(NbtCompound tag) {
         if (connectionTo != null) {
             tag.put("ConnectionTo", BezierConnection.CODEC.encodeStart(NbtOps.INSTANCE, connectionTo)
                     .get().orThrow());
@@ -91,8 +90,8 @@ public class HypertubeBlockEntity extends BlockEntity implements IBezierProvider
     }
 
     @Override
-    public void load(@NotNull CompoundTag tag) {
-        super.load(tag);
+    public void readNbt(@NotNull NbtCompound tag) {
+        super.readNbt(tag);
 
         if (tag.contains("ConnectionTo")) {
             this.connectionTo = BezierConnection.CODEC.parse(NbtOps.INSTANCE, tag.get("ConnectionTo"))
@@ -111,17 +110,17 @@ public class HypertubeBlockEntity extends BlockEntity implements IBezierProvider
         // Primeiro, determinar as direções possíveis baseadas no estado do bloco
         List<Direction> possibleDirections = new ArrayList<>();
 
-        boolean eastWest = Boolean.TRUE.equals(getBlockState().getValue(HypertubeBlock.EAST_WEST));
+        boolean eastWest = Boolean.TRUE.equals(getCachedState().get(HypertubeBlock.EAST_WEST));
         if (eastWest) {
             possibleDirections.addAll(List.of(Direction.EAST, Direction.WEST));
         }
 
-        boolean upDown = Boolean.TRUE.equals(getBlockState().getValue(HypertubeBlock.UP_DOWN));
+        boolean upDown = Boolean.TRUE.equals(getCachedState().get(HypertubeBlock.UP_DOWN));
         if (upDown) {
             possibleDirections.addAll(List.of(Direction.UP, Direction.DOWN));
         }
 
-        boolean northSouth = Boolean.TRUE.equals(getBlockState().getValue(HypertubeBlock.NORTH_SOUTH));
+        boolean northSouth = Boolean.TRUE.equals(getCachedState().get(HypertubeBlock.NORTH_SOUTH));
         if (northSouth) {
             possibleDirections.addAll(List.of(Direction.NORTH, Direction.SOUTH));
         }
@@ -134,7 +133,7 @@ public class HypertubeBlockEntity extends BlockEntity implements IBezierProvider
         // Agora, remover direções que não podem ser conectadas
         possibleDirections.removeIf(direction -> {
             // Remove se já tem um HypertubeBlock adjacente
-            if (level.getBlockState(worldPosition.relative(direction)).getBlock() instanceof HypertubeBlock) {
+            if (world.getBlockState(pos.offset(direction)).getBlock() instanceof HypertubeBlock) {
                 return true;
             }
 
@@ -145,10 +144,10 @@ public class HypertubeBlockEntity extends BlockEntity implements IBezierProvider
 
             // Remove se já tem uma conexão ativa chegando dessa direção
             if (connectionFrom != null) {
-                BlockEntity blockEntity = level.getBlockEntity(connectionFrom.pos());
+                BlockEntity blockEntity = world.getBlockEntity(connectionFrom.pos());
                 if (blockEntity instanceof HypertubeBlockEntity hypertubeBlockEntity
                     && hypertubeBlockEntity.getConnectionTo() != null
-                    && hypertubeBlockEntity.getConnectionTo().getToPos().pos().equals(this.worldPosition)
+                    && hypertubeBlockEntity.getConnectionTo().getToPos().pos().equals(this.pos)
                     && hypertubeBlockEntity.getConnectionTo().getToPos().direction().getOpposite().equals(direction)) {
                     return true;
                 }
@@ -161,33 +160,39 @@ public class HypertubeBlockEntity extends BlockEntity implements IBezierProvider
     }
 
     @Override
-    public @NotNull CompoundTag getUpdateTag() {
-        CompoundTag tag = super.getUpdateTag();
-        saveAdditional(tag);
+    public @NotNull NbtCompound toInitialChunkDataNbt() {
+        NbtCompound tag = super.toInitialChunkDataNbt();
+        writeNbt(tag);
         return tag;
     }
 
     @Override
-    public @NotNull ClientboundBlockEntityDataPacket getUpdatePacket() {
-        return ClientboundBlockEntityDataPacket.create(this);
+    public @NotNull BlockEntityUpdateS2CPacket toUpdatePacket() {
+        return BlockEntityUpdateS2CPacket.create(this);
     }
 
-    @Override
-    public void onDataPacket(Connection net, ClientboundBlockEntityDataPacket pkt) {
-        CompoundTag tag = pkt.getTag();
+    // Unneeded in Fabric
+    /* @Override
+    public void onDataPacket(ClientConnection net, BlockEntityUpdateS2CPacket pkt) {
+        NbtCompound tag = pkt.getNbt();
         if (tag == null) {
             return;
         }
-        load(tag);
-    }
+        readNbt(tag);
+    } */
 
     @Override
     public BezierConnection getBezierConnection() {
         return connectionTo;
     }
 
+    public Box getRenderBounds() {
+        return new Box(pos).expand(512);
+    }
+
+    // For IBezierProvider, BlockEntity on Forge has the getPos method named getBlockPos
     @Override
-    public AABB getRenderBoundingBox() {
-        return new AABB(worldPosition).inflate(512);
+    public BlockPos getBlockPos() {
+        return getPos();
     }
 }
