3d22eff8a8593e7b7fb81fb3ae7d755af9387a05
JXSnack <jx.snack@gmail.com>
2025-06-12 22:42:57 +0200

    port HyperEntranceBlockEntity

--- src/main/java/com/pedrorok/hypertube/blocks/blockentities/HyperEntranceBlockEntity.java
+++ src/main/java/com/pedrorok/hypertube/blocks/blockentities/HyperEntranceBlockEntity.java
@@ -5,19 +5,19 @@ import com.pedrorok.hypertube.managers.TravelManager;
 import com.pedrorok.hypertube.managers.sound.TubeSoundManager;
 import com.simibubi.create.content.kinetics.base.IRotate;
 import com.simibubi.create.content.kinetics.base.KineticBlockEntity;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 
 import java.util.List;
 import java.util.Optional;
@@ -41,24 +41,24 @@ public class HyperEntranceBlockEntity extends KineticBlockEntity {
 
 
     @Override
-    protected void write(CompoundTag compound, boolean clientPacket) {
+    protected void write(NbtCompound compound, boolean clientPacket) {
         super.write(compound, clientPacket);
     }
 
     @Override
-    protected void read(CompoundTag compound, boolean clientPacket) {
+    protected void read(NbtCompound compound, boolean clientPacket) {
         super.read(compound, clientPacket);
     }
 
     @Override
     public void remove() {
-        if (level.isClientSide) {
+        if (world.isClient) {
             removeClient();
         }
         super.remove();
     }
 
-    @OnlyIn(Dist.CLIENT)
+    @Environment(EnvType.CLIENT)
     private void removeClient() {
         TubeSoundManager.getAmbientSound(tubeSoundId).stopSound();
         TubeSoundManager.removeAmbientSound(tubeSoundId);
@@ -67,49 +67,49 @@ public class HyperEntranceBlockEntity extends KineticBlockEntity {
     @Override
     public void tick() {
         super.tick();
-        if (level.isClientSide) {
+        if (world.isClient) {
             tickClient();
             return;
         }
-        BlockState state = this.getBlockState();
-        BlockPos pos = this.getBlockPos();
+        BlockState state = this.getCachedState();
+        BlockPos pos = this.getPos();
 
         float actualSpeed = Math.abs(this.getSpeed());
-        Boolean isOpen = state.getValue(HyperEntranceBlock.OPEN);
+        Boolean isOpen = state.get(HyperEntranceBlock.OPEN);
 
         if (actualSpeed < SPEED_TO_START) {
             if (isOpen) {
-                level.setBlock(pos, state.setValue(HyperEntranceBlock.OPEN, false), 3);
+                world.setBlockState(pos, state.with(HyperEntranceBlock.OPEN, false), 3);
             }
             return;
         }
 
-        Optional<ServerPlayer> nearbyPlayer = getNearbyPlayers((ServerLevel) level, pos.getCenter());
+        Optional<ServerPlayerEntity> nearbyPlayer = getNearbyPlayers((ServerWorld) world, pos.toCenterPos());
         if (nearbyPlayer.isEmpty()) {
             if (isOpen) {
-                level.setBlock(pos, state.setValue(HyperEntranceBlock.OPEN, false), 3);
+                world.setBlockState(pos, state.with(HyperEntranceBlock.OPEN, false), 3);
             }
             return;
         }
         if (!isOpen) {
-            level.setBlock(pos, state.setValue(HyperEntranceBlock.OPEN, true), 3);
+            world.setBlockState(pos, state.with(HyperEntranceBlock.OPEN, true), 3);
         }
 
-        Optional<ServerPlayer> inRangePlayer = getInRangePlayers((ServerLevel) level, pos.getCenter(), state.getValue(HyperEntranceBlock.FACING));
+        Optional<ServerPlayerEntity> inRangePlayer = getInRangePlayers((ServerWorld) world, pos.toCenterPos(), state.get(HyperEntranceBlock.FACING));
         if (inRangePlayer.isEmpty()) return;
 
-        ServerPlayer player = inRangePlayer.get();
-        if (player.isShiftKeyDown()) return;
+        ServerPlayerEntity player = inRangePlayer.get();
+        if (player.isSneaking()) return;
         TravelManager.tryStartTravel(player, pos, state, actualSpeed / 512);
     }
 
 
-    @OnlyIn(Dist.CLIENT)
+    @Environment(EnvType.CLIENT)
     private void tickClient() {
 
         // this is just for sound
-        BlockState state = this.getBlockState();
-        BlockPos pos = this.getBlockPos();
+        BlockState state = this.getCachedState();
+        BlockPos pos = this.getPos();
 
         float actualSpeed = Math.abs(this.getSpeed());
 
@@ -119,25 +119,25 @@ public class HyperEntranceBlockEntity extends KineticBlockEntity {
             return;
         }
 
-        boolean isOpen = state.getValue(HyperEntranceBlock.OPEN);
+        boolean isOpen = state.get(HyperEntranceBlock.OPEN);
 
-        LocalPlayer player = Minecraft.getInstance().player;
-        Vec3 source = pos.getCenter();
-        Vec3 listener = player.position();
+        ClientPlayerEntity player = MinecraftClient.getInstance().player;
+        Vec3d source = pos.toCenterPos();
+        Vec3d listener = player.getPos();
 
-        Vec3 worldDirection = source.subtract(listener).normalize();
+        Vec3d worldDirection = source.subtract(listener).normalize();
 
-        Vec3 forward = player.getLookAngle().normalize();
-        Vec3 up = player.getUpVector(1.0F).normalize();
-        Vec3 right = forward.cross(up).normalize();
+        Vec3d forward = player.getRotationVector().normalize();
+        Vec3d up = player.getOppositeRotationVector(1.0F).normalize();
+        Vec3d right = forward.crossProduct(up).normalize();
 
-        double x = worldDirection.dot(right);
-        double y = worldDirection.dot(up);
-        double z = worldDirection.dot(forward);
+        double x = worldDirection.dotProduct(right);
+        double y = worldDirection.dotProduct(up);
+        double z = worldDirection.dotProduct(forward);
 
-        Vec3 rotatedDirection = new Vec3(x, y, z).normalize();
+        Vec3d rotatedDirection = new Vec3d(x, y, z).normalize();
 
-        double distance = player.distanceToSqr(source);
+        double distance = player.squaredDistanceTo(source);
 
         sound.enableClientPlayerSound(
                 player,
@@ -148,24 +148,24 @@ public class HyperEntranceBlockEntity extends KineticBlockEntity {
     }
 
 
-    private Optional<ServerPlayer> getInRangePlayers(ServerLevel level, Vec3 centerPos, Direction facing) {
-        return level.players().stream()
+    private Optional<ServerPlayerEntity> getInRangePlayers(ServerWorld level, Vec3d centerPos, Direction facing) {
+        return level.getPlayers().stream()
                 .filter(player -> player.getBoundingBox()
-                        .inflate(RADIUS - 0.25)
-                        .contains(centerPos.add(Vec3.atLowerCornerOf(facing.getOpposite().getNormal()))))
+                        .expand(RADIUS - 0.25)
+                        .contains(centerPos.add(Vec3d.of(facing.getOpposite().getVector()))))
                 .findFirst();
     }
 
-    private Optional<ServerPlayer> getNearbyPlayers(ServerLevel level, Vec3 centerPos) {
-        return level.players().stream()
+    private Optional<ServerPlayerEntity> getNearbyPlayers(ServerWorld level, Vec3d centerPos) {
+        return level.getPlayers().stream()
                 .filter(player -> player.getBoundingBox()
-                        .inflate(RADIUS * 3)
+                        .expand(RADIUS * 3)
                         .contains(centerPos))
                 .findFirst();
     }
 
     @Override
-    public boolean addToGoggleTooltip(List<Component> tooltip, boolean isPlayerSneaking) {
+    public boolean addToGoggleTooltip(List<Text> tooltip, boolean isPlayerSneaking) {
         super.addToGoggleTooltip(tooltip, isPlayerSneaking);
         IRotate.SpeedLevel.getFormattedSpeedText(speed, speed < 16)
                 .forGoggles(tooltip);
