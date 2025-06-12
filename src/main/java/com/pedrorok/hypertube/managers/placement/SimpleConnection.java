dbaa1db5b1c524355bb8dd21dcd67c6ed315c33d
JXSnack <jx.snack@gmail.com>
2025-06-12 19:10:33 +0200

    port SimpleConnection

--- src/main/java/com/pedrorok/hypertube/managers/placement/SimpleConnection.java
+++ src/main/java/com/pedrorok/hypertube/managers/placement/SimpleConnection.java
@@ -2,8 +2,8 @@ package com.pedrorok.hypertube.managers.placement;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 
 public record SimpleConnection(BlockPos pos, Direction direction) {
     public static final Codec<SimpleConnection> CODEC = RecordCodecBuilder.create(i -> i.group(
