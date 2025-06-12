0053f07d41b7991ebeb8b60ecb3527afa8222481
JXSnack <jx.snack@gmail.com>
2025-06-12 22:59:36 +0200

    port EntranceBlockEntityRenderer

--- src/main/java/com/pedrorok/hypertube/client/renderer/EntranceBlockEntityRenderer.java
+++ src/main/java/com/pedrorok/hypertube/client/renderer/EntranceBlockEntityRenderer.java
@@ -1,17 +1,17 @@
 package com.pedrorok.hypertube.client.renderer;
 
-import com.mojang.blaze3d.vertex.PoseStack;
 import com.pedrorok.hypertube.blocks.HyperEntranceBlock;
 import com.pedrorok.hypertube.blocks.blockentities.HyperEntranceBlockEntity;
 import com.pedrorok.hypertube.registry.ModPartialModels;
 import com.simibubi.create.content.kinetics.base.KineticBlockEntityRenderer;
 import com.simibubi.create.foundation.render.CachedBufferer;
 import com.simibubi.create.foundation.render.SuperByteBuffer;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
-import net.minecraft.core.Direction;
-import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.Direction;
 
 /**
  * @author Rok, Pedro Lucas nmm. Created on 02/06/2025
@@ -19,27 +19,27 @@ import net.minecraft.world.level.block.state.BlockState;
  */
 public class EntranceBlockEntityRenderer extends KineticBlockEntityRenderer<HyperEntranceBlockEntity> {
 
-    public EntranceBlockEntityRenderer(BlockEntityRendererProvider.Context context) {
+    public EntranceBlockEntityRenderer(BlockEntityRendererFactory.Context context) {
         super(context);
     }
 
     @Override
-    protected void renderSafe(HyperEntranceBlockEntity be, float partialTicks, PoseStack ms, MultiBufferSource buffer,
+    protected void renderSafe(HyperEntranceBlockEntity be, float partialTicks, MatrixStack ms, VertexConsumerProvider buffer,
                               int light, int overlay) {
 
-        BlockState blockState = be.getBlockState();
+        BlockState blockState = be.getCachedState();
         if (!(blockState.getBlock() instanceof HyperEntranceBlock)) {
             return;
         }
 
-        Direction facing = blockState.getValue(HyperEntranceBlock.FACING);
+        Direction facing = blockState.get(HyperEntranceBlock.FACING);
         SuperByteBuffer cogwheelModel = CachedBufferer.partialFacingVertical(ModPartialModels.COGWHEEL_HOLE, blockState, facing);
 
-        float angle = getAngleForTe(be, be.getBlockPos(), facing.getAxis());
+        float angle = getAngleForTe(be, be.getPos(), facing.getAxis());
         Direction.Axis rotationAxisOf = getRotationAxisOf(be);
 
 
         kineticRotationTransform(cogwheelModel, be, rotationAxisOf, angle, light);
-        cogwheelModel.renderInto(ms, buffer.getBuffer(RenderType.solid()));
+        cogwheelModel.renderInto(ms, buffer.getBuffer(RenderLayer.getSolid()));
     }
 }
